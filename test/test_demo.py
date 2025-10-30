4e283f41418b1f16f6245e79c2f58eb193a4513b
sriram <kondaveetimohan2003@gmail.com>
2025-10-30 16:06:55 +0530

    write the CI demo code

---
+++ test/test_demo.py
@@ -0,0 +1,8 @@
+from src.mathfun import add,sub
+def test_add():
+    assert add(2,3) == 5
+    assert add(0,0) == 0
+def test_sub():    
+    assert sub(2,3) == -1
+    assert sub(2,3) == 1
+print("passs")
